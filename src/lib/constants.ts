7a5fe0b513790de2d152b6e465f14d77f7fca567
plouricio <mauricioespinosa73@gmail.com>
2025-08-14 18:33:39 -0400

    Update API base URLs in constants.ts to point to production endpoints for Sofia management services.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 // API URLs
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4900';
-export const API_BASE_SOFIA = import.meta.env.VITE_API_CRUD_URL || 'http://localhost:4500'; 
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://apis.sofiagestionagricola.cl/api-cruds'; //'http://localhost:4900';
+export const API_BASE_SOFIA = import.meta.env.VITE_API_CRUD_URL || 'https://apis.sofiagestionagricola.cl/api-sofia'; //'http://localhost:4500'; 
 // API endpoints
 export const ENDPOINTS = {
   productCategory: {
